7e81765961983850f9f11be6c14c81152902d14e
Infoser <issuatstudy090@gmail.com>
2024-05-16 09:39:45 +0530

    hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,10 @@
 
 int main ()
 {
-
-    printf("This is Infoser090");
-    printf("This is Infoser090");
+    int x, y;
+    scanf("%i %i", &x,&y);
+    int z = (x+y)/2;
+    printf("%i\n",z);
+    printf("This is Infoser090\n");
+    printf("This is Infoser090\n");
 }
